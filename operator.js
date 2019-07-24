f492ef80eeb22b678bec3eb53c1d4853d8a957fe
iamisafi <isafi@gmail.com>
2019-07-24 20:21:28 +0600

    add operator

---
+++ operator.js
@@ -0,0 +1,33 @@
+// categories of operators
+
+// Assignment =
+
+// Arithmatic + - * /
+
+// var m = 10 % 3;
+// console.log(m);
+
+// Increment / Decrement ++ --
+// var a = 1;
+// a++;
+// console.log(++a);
+// console.log(a);
+
+// String '' +
+
+// Precedence
+
+// var b = 2 + 2 * 3;
+// console.log(b);
+
+// Function invocation operators ()
+//console.log('');
+
+// logical and: && or: ||
+
+// Member access operator .
+// console.log
+
+// code block operators
+
+// Array element access operators []
