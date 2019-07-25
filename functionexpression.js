ef5d708455b8e16db30872a3fc7b9df5664004fa
iamisafi <isafi@gmail.com>
2019-07-25 14:57:58 +0600

    add functionexpression

---
+++ functionexpression.js
@@ -0,0 +1,24 @@
+// setTimeout(function () {
+//
+// console.log('I waited 2 seconds');
+// }, 2000);
+
+// 2nd part
+
+// let counter = 0;
+// function timeout(){
+//    setTimeout(function () {
+//      console.log('hi ' + counter++);
+//      timeout();
+//    }, 2000);
+// }
+//
+// timeout();
+
+// press cntr + c for stop to run
+
+// part 3
+
+(function(){
+  console.log('Immediately Invoked Function Expression (IIFE)');
+})();
